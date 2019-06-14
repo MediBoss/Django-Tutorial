444484d1f7c94ee59c140632c058183314f181fb
Medi Assumani <mediassumani49@gmail.com>
2019-06-14 11:23:58 -0700

    [Update] completed part 4 of tutorial

--- demo/polls/views.py
+++ demo/polls/views.py
@@ -1,26 +1,45 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.views import generic
 from django.urls import reverse
 from .models import Question, Choice
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-# View(controller) to handle the page that shows detail of a question
-def detail(request, question_id):
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
 
-# View(controller) to handle the page that shows results of of a question
-def results(request, question_id):
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
+
+
+# # View(controller) to handle the page that shows detail of a question
+# def detail(request, question_id):
+
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+
+# # View(controller) to handle the page that shows results of of a question
+# def results(request, question_id):
+
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
 
 # View(controller) to handle the page that shows the vote of a question
 def vote(request, question_id):
